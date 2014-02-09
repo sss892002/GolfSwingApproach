80a8bc33192236a10e5447c12ae21fbd36f84ca3
Tae Kim <tae@zikto.com>
2014-02-09 22:32:42 +0900

    Successfully Log BT data

--- golfswingapproach/src/com/zikto/golfswingapproach/MainActivity.java
+++ golfswingapproach/src/com/zikto/golfswingapproach/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.UUID;
 
 import com.example.golfswingapproach.R;
@@ -13,6 +15,7 @@ import com.example.golfswingapproach.R;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -31,14 +34,16 @@ public class MainActivity extends Activity {
 	private BluetoothAdapter btAdapter = null;
 	private BluetoothSocket btSocket = null;
 	private OutputStream outStream = null;
+	private InputStream inStream = null;
+	private BluetoothThread btThread;
 
 	// Well known SPP UUID
 	private static final UUID MY_UUID =
 			UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 	// Insert your server's MAC address
-	private static String address = "00:00:00:00:00:00";
-	private final ListAdapter mNewDevicesArrayAdapter = new ArrayAdapter<String>(this, R.id.new_devices);
+	private static String address = "00:A0:96:3B:E5:4D";
+	//	private final ListAdapter mNewDevicesArrayAdapter = new ArrayAdapter<String>(this, R.id.new_devices);
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -50,22 +55,62 @@ public class MainActivity extends Activity {
 		out.append("\n...In onCreate()...");
 
 		btAdapter = BluetoothAdapter.getDefaultAdapter();
-
-
 		//Create a reciever for the Intent
 
 		//IntentFilter will match the action specified
-		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		//IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
 		//broadcast reciever for any matching filter
-		this.registerReceiver(mReciever, filter);
+		//this.registerReceiver(mReciever, filter);
 
-		filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-		this.registerReceiver(mReciever, filter);
+		//filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+		//this.registerReceiver(mReciever, filter);
 
-		ListView foundDevicesListView = (ListView) findViewById(R.id.new_devices);
-		foundDevicesListView.setAdapter(mNewDevicesArrayAdapter);
+		//ListView foundDevicesListView = (ListView) findViewById(R.id.new_devices);
+		//foundDevicesListView.setAdapter(mNewDevicesArrayAdapter);
 
 		CheckBTState();
+		
+		
+		out.append("\n...In onResume...\n...Attempting client connect...");
+
+		//TODO : need to search and choose
+		//btAdapter.startDiscovery();
+
+		// Set up a pointer to the remote node using it's address.
+		BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+		// Two things are needed to make a connection:
+		//   A MAC address, which we got above.
+		//   A Service ID or UUID.  In this case we are using the
+		//     UUID for SPP.
+		try {
+			btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+		} catch (IOException e) {
+			AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+		}
+
+		// Discovery is resource intensive.  Make sure it isn't going on
+		// when you attempt to connect and pass your message.
+		btAdapter.cancelDiscovery();
+
+		// Establish the connection.  This will block until it connects.
+		try {
+			btSocket.connect();
+			out.append("\n...Connection established and data link opened..\n");
+		} catch (IOException e) {
+			try {
+				btSocket.close();
+			} catch (IOException e2) {
+				AlertBox("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+			}
+		}
+
+		
+		//TODO : Read data packet
+		
+		btThread = new BluetoothThread(btSocket);
+		btThread.start();
+
 	}
 
 	@Override
@@ -73,68 +118,73 @@ public class MainActivity extends Activity {
 		super.onStart();
 		out.append("\n...In onStart()...");
 	}
-
+	
 	@Override
 	public void onResume() {
 		super.onResume();
-
+/*
 		out.append("\n...In onResume...\n...Attempting client connect...");
 
 		//TODO : need to search and choose
-		btAdapter.startDiscovery();
+		//btAdapter.startDiscovery();
 
+		// Set up a pointer to the remote node using it's address.
+		BluetoothDevice device = btAdapter.getRemoteDevice(address);
 
+		// Two things are needed to make a connection:
+		//   A MAC address, which we got above.
+		//   A Service ID or UUID.  In this case we are using the
+		//     UUID for SPP.
+		try {
+			btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+		} catch (IOException e) {
+			AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+		}
 
-		//    // Set up a pointer to the remote node using it's address.
-		//    BluetoothDevice device = btAdapter.getRemoteDevice(address);
-		// 
-		//    // Two things are needed to make a connection:
-		//    //   A MAC address, which we got above.
-		//    //   A Service ID or UUID.  In this case we are using the
-		//    //     UUID for SPP.
-		//    try {
-		//      btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-		//    } catch (IOException e) {
-		//      AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
-		//    }
-		// 
-		//    // Discovery is resource intensive.  Make sure it isn't going on
-		//    // when you attempt to connect and pass your message.
-		//    btAdapter.cancelDiscovery();
-		// 
-		//    // Establish the connection.  This will block until it connects.
-		//    try {
-		//      btSocket.connect();
-		//      out.append("\n...Connection established and data link opened...");
-		//    } catch (IOException e) {
-		//      try {
-		//        btSocket.close();
-		//      } catch (IOException e2) {
-		//        AlertBox("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
-		//      }
-		//    }
-		// 
-		//    // Create a data stream so we can talk to server.
-		//    out.append("\n...Sending message to server...");
-		// 
-		//    try {
-		//      outStream = btSocket.getOutputStream();
-		//    } catch (IOException e) {
-		//      AlertBox("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
-		//    }
-		// 
-		//    String message = "Hello from Android.\n";
-		//    byte[] msgBuffer = message.getBytes();
-		//    try {
-		//      outStream.write(msgBuffer);
-		//    } catch (IOException e) {
-		//      String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
-		//      if (address.equals("00:00:00:00:00:00")) 
-		//        msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
-		//      msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
-		//       
-		//      AlertBox("Fatal Error", msg);       
-		//    }
+		// Discovery is resource intensive.  Make sure it isn't going on
+		// when you attempt to connect and pass your message.
+		btAdapter.cancelDiscovery();
+
+		// Establish the connection.  This will block until it connects.
+		try {
+			btSocket.connect();
+			out.append("\n...Connection established and data link opened..\n");
+		} catch (IOException e) {
+			try {
+				btSocket.close();
+			} catch (IOException e2) {
+				AlertBox("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+			}
+		}
+
+		
+		//TODO : Read data packet
+		
+		btThread = new BluetoothThread(btSocket);
+		btThread.start();
+		// Create a data stream so we can talk to server.
+		//		out.append("\n...Sending message to server...");
+		//
+		//		try {
+		//			outStream = btSocket.getOutputStream();
+		//		} catch (IOException e) {
+		//			AlertBox("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+		//		}
+		//
+		//		String message = "Hello from Android.\n";
+		//		byte[] msgBuffer = message.getBytes();
+		//		try {
+		//			outStream.write(msgBuffer);
+		//		} catch (IOException e) {
+		//			String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+		//			if (address.equals("00:00:00:00:00:00")) 
+		//				msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
+		//			msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+		//
+		//			AlertBox("Fatal Error", msg);       
+		//		}
+		 * */
+		 
 	}
 
 	@Override
@@ -143,19 +193,6 @@ public class MainActivity extends Activity {
 
 		out.append("\n...In onPause()...");
 
-		if (outStream != null) {
-			try {
-				outStream.flush();
-			} catch (IOException e) {
-				AlertBox("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
-			}
-		}
-
-		try     {
-			btSocket.close();
-		} catch (IOException e2) {
-			AlertBox("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
-		}
 	}
 
 	@Override
@@ -211,9 +248,11 @@ public class MainActivity extends Activity {
 				BluetoothDevice btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 				//	  			
 				if(btDevice.getBondState() != BluetoothDevice.BOND_BONDED){
-
-					((ArrayAdapter<String>) mNewDevicesArrayAdapter).add(btDevice.getName()+"\n"+btDevice.getAddress());
+					//TODO : find and MAC 
+					out.append(btDevice.getName()+"\n"+btDevice.getAddress());
+					//((ArrayAdapter<String>) mNewDevicesArrayAdapter).add(btDevice.getName()+"\n"+btDevice.getAddress());
 				}
+				out.append(btDevice.getName()+"\n"+btDevice.getAddress());
 				out.append("ACTION_FOUND\n");
 			}
 			else{
@@ -231,6 +270,51 @@ public class MainActivity extends Activity {
 
 		}
 	};
+	public class BluetoothThread extends Thread{
+		private final BluetoothSocket socket;
+		private InputStream inStream;
+		public BluetoothThread(BluetoothSocket socket)
+		{
+			this.socket = socket;
+			try{
+				inStream = socket.getInputStream();
+			} catch(IOException e)
+			{
+				Log.e("TAG", "socket created failed",e);
+			}
+
+		}
+
+		public void run()
+		{
+			int bytes;
+			byte[] buffer = new byte[30];
+
+			while(true)
+			{
+				try
+				{
+					bytes = inStream.read(buffer);
+					String message =  Arrays.toString(buffer);
+					//out.append("Data : ");
+					
+					if( buffer[0] == 36)
+						Log.d("BT1",message);	
+					
+//					for (int i =  0 ; i < bytes ; i++)
+//					{
+//						//out.append(" "+ buffer[i]);
+//						Log.d("BT1",buffer[i]);
+//					}
+					//out.append("\n");
+					
+				}catch(IOException e )
+				{
+					AlertBox("IO Exception",e.getMessage());
+				}
+			} 
+		}
+	}
 }
 
 
