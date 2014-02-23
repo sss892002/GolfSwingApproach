af0370a4d520e75d197ec29297a2e822eaa8cc53
Tae Kim <tae@zikto.com>
2014-02-23 16:21:02 +0900

    Remove other
    Add Packet Parser Class

---
+++ golfswingapproach/src/com/zikto/invensense/utils/PacketParser.java
@@ -0,0 +1,17 @@
+package com.zikto.invensense.utils;
+
+import java.util.ArrayList;
+
+public class PacketParser {
+
+	private ArrayList<Byte> data;
+	
+	
+ 	public PacketParser(ArrayList<Byte> data)
+	{
+		this.data = data;
+		
+	}
+ 	
+ 	
+}
